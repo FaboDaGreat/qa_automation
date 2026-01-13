2ca7b3ff7c2f0abe341cac1a3adf89518b1f738d
FaboDaGreat <fabian.richmond92@gmail.com>
2026-01-12 20:24:16 -0500

    Updated test. Added a cross-check and an additional helper function along with it to improve the strength of the test

--- tests/hackerNews.test.js
+++ tests/hackerNews.test.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import validateLinks from "../helper_functions/validateLinksFunction";
+import calculateSeconds from "../helper_functions/calculateSecondsFunction";
 
 // Choose the exact amount of articles you want validated
 const articleCount = 100;
@@ -21,10 +22,11 @@ test(`Validating first ${articleCount} Hacker Articles`, async ({ page }) => {
         // Asserts that the pages that are supposed to contain the most recent Hacker articles are the ones being tested
         await expect(page).toHaveTitle("New Links | Hacker News");
 
-        // Constructs objects containing each article's ID # and the link to the site containing the article
+        // Constructs objects containing each article's site ID #, the link to the article, and the timestamp stating how long ago the article was posted
         const articles = await page.locator('tr.athing').evaluateAll(nodes => nodes.map(n => ({
             id: Number(n.id),
-            link: n.querySelector('span.titleline > a').href
+            link: n.querySelector('span.titleline > a').href,
+            timeAgoPosted: n.nextElementSibling.querySelector('span.age').innerText
         })));
 
         allArticles.push(...articles);
@@ -34,16 +36,29 @@ test(`Validating first ${articleCount} Hacker Articles`, async ({ page }) => {
 
     const testingArticles = allArticles.slice(0, articleCount);
 
-    // Makes sure that there is now an array of just exactly the amount of articles wanting to be validated and that none of the site ID numbers were duplicated
-    expect(new Set(testingArticles
-        .map(a => a.id)).size,
-        "Array has exact amount of articles requested with no duplicate IDs").toBe(articleCount);
+    // Makes sure that there is now an array of exactly the amount of articles requested to be validated and that none of the site ID numbers were duplicated
+    expect(new Set(testingArticles.map(a => a.id)).size,
+        "Array has exact amount of articles requested with no duplicate IDs")
+        .toBe(articleCount);
+
+    // Uses callback function to create an array of how long ago each article was posted in seconds 
+    const convertedTimes = calculateSeconds(testingArticles);
+
+    // Validates time ago strings were in order on the site from least to greatest (newest to oldest article)
+    const isNewestToOldest = convertedTimes.every((v, i, a) =>
+        i === 0 || a[i - 1] <= v
+    );
+
+    expect(isNewestToOldest,
+        "Articles on Hacker News are already in order from newest to oldest according to their visible timestamps")
+        .toBe(true);
 
     const sortedArticles = testingArticles.toSorted((a, b) => b.id - a.id);
 
     // Validates that the articles collected were already in order on the site from greatest to least ID number (newest to oldest article)
     expect(testingArticles,
-        "Articles on Hacker News are already in order from newest to oldest").toEqual(sortedArticles);
+        "Articles on Hacker News are already ordered newest to oldest by their ID")
+        .toEqual(sortedArticles);
 
     // Invokes callback function to validate that all the links for each article collected are working properly
     await validateLinks(testingArticles, page);
